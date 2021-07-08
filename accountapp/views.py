6c29c690b74c7d62315423b0d6d7808f07101ccc
sara1060 <widejks96@gmail.com>
2021-07-08 16:38:01 +0900

    2th uebung_3

--- accountapp/views.py
+++ accountapp/views.py
@@ -2,11 +2,19 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from accountapp.models import MoinMoin
 
 def wasdenn(request):
     if request.method=='POST':
+
+        temp = request.POST.get('MoinMoin_input')
+
+        new_moinmoin = MoinMoin()
+        new_moinmoin.text = temp
+        new_moinmoin.save()
+
            return render(request, 'accountapp/MOINMOIN.html',
-                         context={'text':' POST METHOD!'})
+                         context={'moinmoin_output':new_moinmoin})
     else:
            return render(request,'accountapp/MOINMOIN.html',
                          context={'text':' POST METHOD!'})
