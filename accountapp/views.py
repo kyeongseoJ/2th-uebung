5c98745eb0465f03abff9ce32171c688375914e0
sara1060 <widejks96@gmail.com>
2021-07-08 15:16:40 +0900

    2th uebung_2

--- accountapp/views.py
+++ accountapp/views.py
@@ -3,5 +3,10 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def wasdenn (request):
-    return render(request, 'accountapp/MOINMOIN.html')
+def wasdenn(request):
+    if request.method=='POST':
+           return render(request, 'accountapp/MOINMOIN.html',
+                         context={'text':' POST METHOD!'})
+    else:
+           return render(request,'accountapp/MOINMOIN.html',
+                         context={'text':' POST METHOD!'})
