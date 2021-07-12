ab7dfe2764f23cc53ff3e2ae405560a05db52a10
kyeongseoj <kyeongseojeon@likelion.org>
2021-07-12 16:31:56 +0900

    2th uebung_6

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from accountapp.views import wasdenn
+from accountapp.views import wasdenn, AccountCreateView
 
 app_name = 'accountapp'
 
 urlpatterns = [
-    path('wasdenn/', wasdenn, name='wasdenn')
+    path('wasdenn/', wasdenn, name='wasdenn'),
+
+    path('create/', AccountCreateView.as_view(), name='create')
 ]
