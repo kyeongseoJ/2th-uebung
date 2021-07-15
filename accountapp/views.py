6bdcac8b9370776cd7a283dd55dc7aef13123449
kyeongseoj <kyeongseojeon@likelion.org>
2021-07-15 15:30:39 +0900

    2th uebung_12

--- accountapp/views.py
+++ accountapp/views.py
@@ -30,3 +30,8 @@ class AccountCreateView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('accountapp:wasdenn')
     template_name = 'accountapp/create.html'
+
+class AccountDetailView(DetailView):
+    model = User
+    context_object_name = 'target_user'
+    template_name = 'accountapp/detail.html'
