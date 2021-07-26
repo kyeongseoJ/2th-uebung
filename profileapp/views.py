f9cce7fbf8e07a65434645dec1eff15adbd32686
kyeongseoj <kyeongseojeon@likelion.org>
2021-07-26 15:48:34 +0900

    2th uebung_27

--- profileapp/views.py
+++ profileapp/views.py
@@ -9,5 +9,11 @@ from profileapp.models import Profile
 class ProfileCreateView(CreateView):
     model = Profile
     form_class = ProfileCreationForm
-    success_url = reverse_lazy('accountapp:hello_world')
+    success_url = reverse_lazy('accountapp:wasdenn')
     template_name = 'profileapp/create.html'
+
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
