71bd4816726044772c7f713fdbd945f3763ae9c7
kyeongseoj <kyeongseojeon@likelion.org>
2021-07-28 10:40:28 +0900

    2th uebung_32

--- accountapp/views.py
+++ accountapp/views.py
@@ -35,8 +35,10 @@ def wasdenn(request):
 class AccountCreateView(CreateView):
     model = User
     form_class = UserCreationForm
-    success_url = reverse_lazy('accountapp:wasdenn')
     template_name = 'accountapp/create.html'
+
+    def get_success_url(self):
+        return reverse('accountapp:detail', kwargs={'pk' : self.object.pk})
 
 
 class AccountDetailView(DetailView):
@@ -53,8 +55,10 @@ class AccountUpdateView(UpdateView):
     model = User
     form_class = AccountCreationForm
     context_object_name = 'target_user'
-    success_url =reverse_lazy('accountapp:wasdenn')
     template_name = 'accountapp/update.html'
+
+    def get_success_url(self):
+        return reverse('accountapp:detail', kwargs={'pk' : self.object.pk})
 
 
 @method_decorator(has_ownership, 'get')
